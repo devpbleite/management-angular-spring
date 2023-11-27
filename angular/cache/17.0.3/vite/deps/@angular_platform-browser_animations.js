445811d961febab6834bae6680318b6285437c0a
Pablo Leite <pabloleite03@gmail.com>
2023-11-27 17:17:27 -0300

    Add files via upload

---
+++ angular/cache/17.0.3/vite/deps/@angular_platform-browser_animations.js
@@ -0,0 +1,21 @@
+import {
+  BrowserAnimationsModule,
+  InjectableAnimationEngine,
+  NoopAnimationsModule,
+  provideAnimations,
+  provideNoopAnimations
+} from "./chunk-76BOX7TI.js";
+import "./chunk-MFPE4VZZ.js";
+import "./chunk-XUYKTPYR.js";
+import {
+  ANIMATION_MODULE_TYPE
+} from "./chunk-UWMPAZLN.js";
+export {
+  ANIMATION_MODULE_TYPE,
+  BrowserAnimationsModule,
+  NoopAnimationsModule,
+  provideAnimations,
+  provideNoopAnimations,
+  InjectableAnimationEngine as ɵInjectableAnimationEngine
+};
+//# sourceMappingURL=@angular_platform-browser_animations.js.map
